cfa000fab77472733bc16c2f51b019b727d294b1
Biorth <zajacmaciej01@gmail.com>
2025-08-01 19:14:46 +0200

    pełniejsza funkcja main menu

--- main.py
+++ main.py
@@ -1,47 +1,75 @@
-def display_menu():
+import random
+
+def display_menu(functions_list: list):
     print()
     print("="*10, "MENU", "="*10)
+    print("0 - Exit")
+    for function in functions_list:
+        command = functions_list.index(function) + 1
+        name = function.__name__.replace("_", " ").title()
+        print(f"{command} - {name}")
 
-    print("1 - Wypisz 'n' pierwszych liczb pierwszych")
-    print("2 - Narysuj kwadrat o długości 'n'")
-    print("3 - Wylosuj męskie lub żeńskie imię")
-    print()
 
-
-def prime_num(n: int) -> list:
-    licznik = 0
-    liczba = 2
+# Tu można wklejać funkcje
+def prime_numbers():
+    print("Wypiszę wszystkie liczby pierwsze w podanym zakresie.")
+    start = int(input("Podaj początek zakresu: "))
+    end = int(input("Podaj koniec zakresu: "))
     liczby_pierwsze = []
-    while licznik < n:
-        for i in range(2, int(liczba ** 0.5) + 1):
-            if liczba % i == 0:
+
+    if start > end:
+        start, end = end, start
+
+    if start < 2:
+        start = 2
+
+    if end < 2:
+        end = 2
+
+    for j in range(start, end+1):
+        for i in range(2, int(j ** 0.5) + 1):
+            if j % i == 0:
                 break
         else:
-            liczby_pierwsze.append(liczba)
-            licznik += 1
-        liczba += 1
-    return liczby_pierwsze
+            liczby_pierwsze.append(j)
+    print(f"Oto lista liczb pierwszych w podanym zakresie:")
+    print(liczby_pierwsze)
 
-def square(n: int):
-    print("*"*2*n)
-    for i in range(n-2):
-        print("*" + " "*(2*n - 2) + "*")
-    print("*"*2*n)
-
-# def random_name_generator(plec: str) -> str:
-    
-
-display_menu()
-
-option = input(": ")
-
-
-if option == "1":
-    prime_count = int(input("Ile liczb pierwszych wypisać? "))
-    print(f"Oto lista {prime_count} pierwszych liczb pierwszych.")
-    print(prime_num(prime_count))
-
-elif option == "2":
+def print_square():
     bok = int(input("Podaj szerokość kwadratu: "))
-    print()
-    square(bok)
+    print("*"*2*bok)
+    for i in range(bok-2):
+        print("*" + " "*(2*bok - 2) + "*")
+    print("*"*2*bok)
+
+def random_name_generator():
+    plec = input("Czy chcesz wygenerować imię męskie(M) czy żeńskie(F)?: ").upper()
+    
+    fem_names = ['Felicia', 'Ayarissa', 'Freya', 'Claire']
+    mal_names = ['Rob', 'Bob', 'Cody', 'John']
+
+    if plec == 'F':
+        name = random.choice(fem_names)
+    elif plec == 'M':
+        name = random.choice(mal_names)
+
+    print(name)
+
+# Lista, do której wrzucamy funkcje do menu
+list_of_functions = [prime_numbers, print_square, random_name_generator]
+# Podawane funkcje nie przyjmują argumentów - przyjmują wartości wewnątrz przez input użytkownika
+
+while True:
+    display_menu(list_of_functions)
+
+    option = int(input(": "))
+
+    if option > len(list_of_functions) or option < 0:
+        print("Brak funkcji")
+        continue
+    elif option == 0:
+        break
+
+    func_to_run = list_of_functions[option - 1]
+
+    func_to_run()
