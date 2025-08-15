813d6b7b346023b397ddf63f10bca4e5410efd4e
mmHare <134324822+mmHare@users.noreply.github.com>
2025-08-15 22:19:21 +0200

    Do poprzedniego commita

--- src/menu_screens.py
+++ src/menu_screens.py
@@ -1,52 +1,28 @@
 """List of program menu screens"""
 
-import types
 from src.config import *
 from src.database import *
 from src.users import *
 from src.help_functions import *
+from src.menu_functions import show_menu
 
 
-def show_menu(title, options: list):
-    """Printing menu from given options - tuples (name, func)"""
-    if len(options) == 0:
-        return
+def main_menu():
+    """Main menu of the program"""
 
-    while True:
-        clear_screen()
-        print("=" * 10, title.upper(), "=" * 10)
-        for i, option in enumerate(options):
-            print(f"{i + 1}. {option[0]}")
-        print("0. Exit")
+    options = [
+        (lambda: menu_user_log if not user_manager.is_logged else None),
+        ("Configuration settings", config_menu),
+        ("Database settings", db_settings_screen),
+        ("User settings", user_management_screen)
+    ]
 
-        choice = input("Select an option: ").strip()
-
-        if choice in ['0', 'q']:
-            return
-        elif choice.isdigit() and 0 <= int(choice) - 1 < len(options):
-            func_tmp = options[int(choice) - 1][1]
-            if type(func_tmp) == types.FunctionType:
-                clear_screen()
-                func_tmp()
-        else:
-            print("Wrong option. Try again.")
-        print()
-        input("Press ENTER to continue...")
-
-
-def display_menu(functions_list: list):
-    print()
-    print("="*10, "MENU", "="*10)
-    print("0 - Exit")
-    for function in functions_list:
-        command = functions_list.index(function) + 1
-        name = function.__name__.replace("_", " ").title()
-        print(f"{command} - {name}")
-
+    show_menu("Main menu", options, info_top=get_logged_user_info)
+    print("See Ya!")
 
 ########################################################
 
-# Configuration
+
 def config_menu():
     """Method to display configuration settings."""
     options = [
@@ -58,7 +34,6 @@ def config_menu():
     show_menu("Configuration settings", options)
 
 
-# Database
 def db_settings_screen():
     """Method to display database settings."""
     options = [
@@ -71,12 +46,11 @@ def db_settings_screen():
     show_menu("Database settings", options)
 
 
-# Users
 def user_management_screen():
     """Method to display user settings."""
 
     options = [
-        ("Log out" if user_manager.is_logged else "Log in", menu_user_log_in_log_out),
+        (menu_user_log),
         ("Register IP", menu_register_user),
         ("User list", menu_list_users),
         ("Add new user", menu_new_user),
@@ -84,19 +58,3 @@ def user_management_screen():
     ]
 
     show_menu("User settings", options)
-
-
-############################################################
-
-
-def main_menu():
-    """Main menu of the program"""
-
-    options = [
-        ("Configuration settings", config_menu),
-        ("Database settings", db_settings_screen),
-        ("User settings", user_management_screen)
-    ]
-
-    show_menu("Main menu", options)
-    print("See Ya!")
